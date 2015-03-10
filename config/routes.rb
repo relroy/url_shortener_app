22a8a2e7468b8ade8ec15c4859d026b0e46c445c
Robert Elroy <relroy5499@gmail.com>
2015-03-10 13:57:53 -0500

    changed resources:link to links, added delete button to index page, changed edit page to same as new.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
    end
  end
 
-  resources :link
+  resources :links
 
   # get '/:slug' => 'links#redirect'
   
---
+++ spec/models/link_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Link, :type => :model do
+  it 'should standardize target_url by removing http://' do
+    link = Link.new(:slug => 'test', :target_url => 'http://example.com')
+    link.standardize_target_url!
+
+    expect(link.target_url).to eq('example.com')
+  end
+
+  it 'should standardize target_url by removing https://' do
+    link = Link.new(:slug => 'test', :target_url => 'https://example.com')
+    link.standardize_target_url!
+
+    expect(link.target_url).to eq('example.com')
+  end
+
+  it 'should return correct visit count' do
+  link = Link.create(:slug => 'test', :target_url => 'https://example.com')
+  5.times do
+    link.visits.create
+  end
+
+  expect(link.visit_count).to eq(5)
+  end
+end
